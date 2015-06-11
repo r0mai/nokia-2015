feba3a86f6ee149e921cb32af29ac2178547e7aa
Matthias Vegh <matyas.vegh@gmail.com>
2015-06-11 17:22:52 +0200

    Only send my wayward son on if not already moving

--- finals/src/Agent.cpp
+++ finals/src/Agent.cpp
@@ -1,19 +1,29 @@
 #include "Agent.hpp"
 #include "Utasitas.h"
 
+#include <iostream>
+
 using namespace calmare;
 
 void eraseOrders() {
+    std::cerr << "Erasing orders" << std::endl;
     koteg.UtasitSzam = 0;
 }
 
-void sendUnitTo(int x, int y, short unitId) {
-    Utasit_Mozog(unitId, x, y);
+void sendUnitTo(int x, int y, const TEgyseg& unit) {
+    if(unit.AkcioKod == caNincs) {
+        std::cerr<<"Unit " << unit.ID << " was idle, sending to "<<x << " "<< y<<std::endl;
+        Utasit_Mozog(unit.ID, x, y);
+    } else {
+        std::cerr<<"Unit " << unit.ID << " was already in movement"<<std::endl;
+
+    }
 }
 
 TKoteg Agent::getOrders(TJatekos jatekos) {
     eraseOrders();
+    std::cerr << "Working with " << jatekos.EgySzam << " units" << std::endl;
     short myOnlySon = jatekos.Egysegek[0].ID;
-    sendUnitTo(52, 52, myOnlySon);
+    sendUnitTo(52, 52, jatekos.Egysegek[0]);
     return koteg;
 }
