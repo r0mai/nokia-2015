78077847c36188aef38a3b84fc70b7d0747b4400
Matthias Vegh <matyas.vegh@gmail.com>
2015-06-11 17:14:49 +0200

    Send my only sun out to the fields

--- finals/src/Agent.cpp
+++ finals/src/Agent.cpp
@@ -7,7 +7,13 @@ void eraseOrders() {
     koteg.UtasitSzam = 0;
 }
 
+void sendUnitTo(int x, int y, short unitId) {
+    Utasit_Mozog(unitId, x, y);
+}
+
 TKoteg Agent::getOrders(TJatekos jatekos) {
     eraseOrders();
+    short myOnlySon = jatekos.Egysegek[0].ID;
+    sendUnitTo(52, 52, myOnlySon);
     return koteg;
 }
