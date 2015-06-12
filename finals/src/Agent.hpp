0444ad9ad279cdcc48c0448b47f83389b3dafe4d
Matyas Vegh <matyas.vegh@ericsson.com>
2015-06-12 06:34:45 +0200

    Research cavalry

--- finals/src/Agent.hpp
+++ finals/src/Agent.hpp
@@ -54,6 +54,7 @@ private:
     void getStuff(Mezo mezo);
 
     bool researchBuildingDefence();
+    bool researchCavalry();
     bool conductBasicResearchTillReachQuantity(short q);
     bool conductBasicResearchTillReachQuantityWithGold(short q);
 
